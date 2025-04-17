341ae848df01ee24756edee8acb73ddcbab89bc8
digambar1411 <deshawaldigambar@gmail.com>
2025-04-17 11:38:26 +0530

    inject childs using react

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-const para = React.createElement('p',{id:'para', className:'para'},'Hello world from React');
+const parent1 = React.createElement(
+  'div',
+  {id:'parent'}, 
+  React.createElement('p',{id:'child'},'Hello world from React'), 
+);
+
+const parent2 = React.createElement(
+  'div',
+  {id:'parent2'},
+  [
+    React.createElement('p',{id:'child'},'child 1 of parent 2'), 
+    React.createElement('p',{id:'child'},'child 2 of parent 2'), 
+  ]
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(para);
+root.render([parent1, parent2]);
